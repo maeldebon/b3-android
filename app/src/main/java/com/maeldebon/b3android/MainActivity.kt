6ee9b7bbb31793dd49488b135901e24b8e603c11
Maël Debon <mael.debon@owkin.com>
2021-03-21 14:47:47 +0100

    Add database with fake entry point

--- app/src/main/java/com/maeldebon/b3android/MainActivity.kt
+++ app/src/main/java/com/maeldebon/b3android/MainActivity.kt
@@ -2,10 +2,24 @@ package com.maeldebon.b3android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val context = this
+
+        this.btn_insert.setOnClickListener {
+            if(etvEuid.text.toString().isNotEmpty()) {
+                val entry = Entry(etvEuid.text.toString().toInt())
+                val db = DatabaseHandler(context)
+                db.insertData(entry)
+            } else {
+                Toast.makeText(context, "Test", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
